67a65009048c7c027960e9906c3b6292b79dd832
Gurdip Bath <bathgurdip1@gmail.com>
2025-01-21 20:39:11 +0000

    enclosed the dashboard component inside the habitprovider so context is available throughout the app

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
+import React from 'react';
+import { HabitProvider } from './context/habitcontext';
 import Dashboard from './components/dashboard/dashboard';
 
 function App() {
   return (
+    <HabitProvider>
     <div className="App">
       <Dashboard /> {/* Render the Dashboard */}
     </div>
+    </HabitProvider>
   );
 }
 
