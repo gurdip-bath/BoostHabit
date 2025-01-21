5fa1524a801d4b7a72709bb89a57877f26fcb664
Gurdip Bath <bathgurdip1@gmail.com>
2025-01-21 18:44:24 +0000

    created FETCH function to fetch data from backend and update the habits state

--- client/src/context/HabitContext.js
+++ client/src/context/HabitContext.js
@@ -7,3 +7,13 @@ export const HabitContext = createContext ();
 export const HabitProvider = ({ children }) => {
     const [habits, setHabits] = useState([]); // Local state to store habits
 }
+
+const fetchHabits = async () => {
+    try {
+        const response = await fetch ('http://localhost:5000/api/habits'); // fetch habits from backend 
+        const data = await response.json(); // convert response into JSON
+        setHabits(data); // Update the habits state with the fetched data
+    } catch (error){
+        console.error('Error fetching habits;', error); // Log errors for debugging
+    }
+}
